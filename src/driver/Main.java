b33e80119af24f134722630c1289e629ce40f41e
MalikHeron <malik.heron2001@gmail.com>
2023-03-10 17:37:57 -0500

    initial commit

--- src/driver/Main.java
+++ src/driver/Main.java
@@ -1,3 +1,5 @@
+package driver;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!, ugh");
---
+++ src/models/registers/Register.java
@@ -0,0 +1,24 @@
+package models.registers;
+
+import java.util.ArrayList;
+
+public class Register {
+
+    private ArrayList arrayList;
+
+    public void increment () {
+        structure.increment();
+    }
+
+    public void decrement () {
+
+    }
+
+    public boolean isEmpty () {
+        if (structure.isEmpty()) {
+
+        } else {
+
+        }
+    }
+}
